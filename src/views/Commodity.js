82d523d1936b141df01c86b7acc87ea062872e3e
Victor <victor.rinnegard@hotmail.com>
2020-11-10 23:16:39 +0000

    Added basic template for chart

--- src/views/Commodity.js
+++ src/views/Commodity.js
@@ -2,10 +2,32 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from "react-router-dom";
 import Header from "../components/Header.js";
 import { Link } from "react-router-dom";
+import {
+  LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+} from 'recharts';
 
 function Commodity(props) {
     const { id } = useParams();
-
+    const data = [
+        {
+            price: 15, time: new Date("2015-03-25T07:00:00Z").getTime()
+        },
+        {
+            price: 18, time: new Date("2015-03-25T11:00:00Z").getTime()
+        },
+        {
+            price: 13, time:  new Date("2015-03-25T14:00:00Z").getTime()
+        },
+        {
+            price: 5, time:  new Date("2015-03-25T17:00:00Z").getTime()
+        },
+        {
+            price: 5, time:  new Date("2015-03-25T21:00:00Z").getTime()
+        },
+        {
+            price: 21, time:  new Date("2015-03-25T23:00:00Z").getTime()
+        },
+    ]
     return (
         <>
             <Header auth={props.auth} />
@@ -21,6 +43,20 @@ function Commodity(props) {
                         </td>
                     </tr>
                 </table>
+                  <LineChart width={600} height={300} data={data}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis
+                        dataKey="time"
+                        type="number"
+                        domain={['dataMin', 'dataMax']}
+                        scale="time"
+                        tickFormatter={(tick) => {return new Date(tick).toTimeString().substring(0, 5);}}
+                    />
+                    <YAxis />
+                    <Line type="monotone" dataKey="price" stroke="#8884d8" activeDot={{ r: 10 }} />
+                    <Tooltip />
+                  </LineChart>
+
             </div>
         </>
     )
