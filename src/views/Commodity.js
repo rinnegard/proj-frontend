1f5100b30c4b57b8c0e592367fc0fc8ad105bf10
Victor <victor.rinnegard@hotmail.com>
2020-11-16 02:59:13 +0000

    Added unconnected to api buying and selling functionally

--- src/views/Commodity.js
+++ src/views/Commodity.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from "react-router-dom";
 import Header from "../components/Header.js";
+import axios from "axios";
 import { Link } from "react-router-dom";
 import {
   LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
@@ -8,6 +9,9 @@ import {
 
 function Commodity(props) {
     const { id } = useParams();
+    const [amount, setAmount] = useState("");
+    const [price, setPrice] = useState(32);
+    const [owned, setOwned] = useState(0);
     const data = [
         {
             price: 15, time: new Date("2015-03-25T07:00:00Z").getTime()
@@ -28,6 +32,43 @@ function Commodity(props) {
             price: 21, time:  new Date("2015-03-25T23:00:00Z").getTime()
         },
     ]
+
+    const authAxios = axios.create({
+        baseURL: "http://localhost:1338",
+        headers: {
+            Authorization: `Bearer ${props.auth.token}`
+        }
+    });
+
+    useEffect(() => {
+        authAxios.get("/user/" + props.auth.id)
+        .then(function(res) {
+            console.log(res.data.doc[id]);
+            setOwned(res.data.doc[id])
+        })
+        .catch(function(error) {
+            console.log(error);
+        })
+    }, []);
+
+
+    function formSubmit(e) {
+        e.preventDefault();
+        console.log(e.nativeEvent.submitter);
+        if (e.nativeEvent.submitter.value === "Buy") {
+            console.log("Buying");
+            setOwned(owned + amount);
+        } else if (e.nativeEvent.submitter.value === "Sell") {
+            console.log("Selling");
+            setOwned(owned - amount);
+        }
+        console.log(owned);
+    }
+
+    function onChange(e) {
+        setAmount(parseInt(e.target.value))
+    }
+
     return (
         <>
             <Header auth={props.auth} />
@@ -37,14 +78,40 @@ function Commodity(props) {
                     <tbody>
                         <tr>
                             <td className="name">
-                                Price
+                                Current Price
                             </td>
                             <td className="value">
-                                52.12
+                                {price}
+                            </td>
+
+                        </tr>
+                        <tr>
+                            <td className="name">
+                                Amount Owned
+                            </td>
+                            <td className="value">
+                                {owned}
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="name">
+                                Total Value
+                            </td>
+                            <td className="value">
+                                {price*owned}
                             </td>
                         </tr>
                     </tbody>
                 </table>
+                <div className="form">
+                    <form onSubmit={formSubmit}>
+                        <label htmlFor="buysell-stock">Amount to buy or sell</label>
+                        <input type="number" name="buysell-stock" required onChange={onChange} value={amount}/>
+                        <p>Cost: {!Number.isNaN(price*amount) && price*amount}</p>
+                        <input className="button" type="submit" value="Buy" />
+                        <input className="button" type="submit" value="Sell" />
+                    </form>
+                </div>
                   <LineChart width={600} height={300} data={data}>
                     <CartesianGrid strokeDasharray="3 3" />
                     <XAxis
