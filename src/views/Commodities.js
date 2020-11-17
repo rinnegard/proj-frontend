cfff0ccfc5cfb9ba132935eb52e9476a5d0134da
Victor <victor.rinnegard@hotmail.com>
2020-11-17 23:00:19 +0000

    Fixed style and added socket to commodoties page

--- src/views/Commodities.js
+++ src/views/Commodities.js
@@ -2,7 +2,42 @@ import React, { useState, useEffect } from 'react';
 import Header from "../components/Header.js";
 import { Link } from "react-router-dom";
 
+import io from 'socket.io-client';
+const socket = io('ws://localhost:1338');
+
 function Commodities(props) {
+    const [goldPrice, setGoldPrice] = useState(0);
+    const [silverPrice, setSilverPrice] = useState(0);
+
+    useEffect(() => {
+        socket.on("connect", function() {
+            console.log("Connected");
+        })
+
+        socket.on("gold", function(item) {
+            console.log(item.price);
+
+            setGoldPrice(item.price)
+        })
+
+        socket.on("silver", function(item) {
+            console.log(item.price);
+
+            setSilverPrice(item.price)
+        })
+
+        socket.on('connect_error', (error) => {
+            console.log(error);
+        });
+
+        socket.on('disconnect', function() {
+            console.info("Disconnected");
+        });
+
+        return () => {
+            socket.off();
+        }
+    }, []);
 
     return (
         <>
@@ -17,11 +52,15 @@ function Commodities(props) {
                         </tr>
                         <tr>
                             <td><Link to="/commodities/gold">Gold</Link></td>
-                            <td>59.12</td>
+                            <td className="value price-value">
+                                {Math.round(goldPrice * 100) / 100}
+                            </td>
                         </tr>
                         <tr>
                             <td><Link to="/commodities/silver">Silver</Link></td>
-                            <td>23.72</td>
+                            <td className="value price-value">
+                                {Math.round(silverPrice * 100) / 100}
+                            </td>
                         </tr>
                     </tbody>
                 </table>
--- src/views/Index.js
+++ src/views/Index.js
@@ -9,11 +9,9 @@ function RegisterPage(props) {
             <div className="content">
                 <h1>Welcome</h1>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tellus nisi, ullamcorper sed gravida eget, cursus ut elit. Aliquam consectetur tortor elit, sit amet porta tortor fermentum id. Morbi at mauris quis magna scelerisque vestibulum nec vel mauris. Nunc hendrerit elementum diam vel fermentum. Aenean mauris massa, pretium a varius non, mattis a justo. Suspendisse dictum porttitor molestie. Nunc blandit nibh in lectus dapibus faucibus. Nunc et ligula porttitor ante placerat facilisis. Morbi orci dui, dapibus in orci quis, dictum facilisis ligula. Aliquam laoreet posuere diam, quis iaculis sem viverra vel. Duis dapibus arcu vel auctor rutrum. Donec sit amet tincidunt enim.
+                    Precious Metals is the premier fake trading website. You can use it to trade in all kinds of fake commodities!
                 </p>
-                <p>
-                    Proin tristique commodo tempus. Nunc efficitur, leo iaculis efficitur sodales, nibh neque vehicula mi, sed volutpat velit justo ultricies justo. Fusce semper ex dui, nec facilisis dolor feugiat non. Ut ut eros ac velit dignissim semper. In dapibus venenatis ipsum eget mattis. Etiam fermentum libero arcu, a sodales quam euismod sit amet. Etiam pellentesque lorem a tellus cursus faucibus. Pellentesque non ultrices felis. Donec ut enim sagittis, tincidunt lorem non, pharetra turpis. Integer posuere ipsum eget egestas blandit. Duis id leo libero. Curabitur blandit tincidunt semper. 
-                </p>
+                <img src="commodities.jpg"/>
             </div>
         </>
     )
