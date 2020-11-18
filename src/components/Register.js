3e9c8647237f77dd90e1295f32e85ed60ec356fe
Victor <victor.rinnegard@hotmail.com>
2020-11-18 01:25:05 +0000

    Added redirect after successful register

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,10 +1,11 @@
 import React, {useState, useEffect} from 'react';
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import axios from "axios";
 
 function Register() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    let history = useHistory();
 
     function formSubmit(e) {
         e.preventDefault();
@@ -17,6 +18,7 @@ function Register() {
         .then(function(res) {
             console.log(res.data.data);
             console.log(res.data.data.message);
+            history.push("/login");
         })
         .catch(function(error) {
             console.log(error);
